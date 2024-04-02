214d3908fe09bc6f4881efb8353bb4d733bc2445
Manthan Sharma <mayank9870866130@gmail.com>
2024-04-02 21:32:49 +0530

    test husky 2

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 function login(username: string): string {
-  return username;
+  const user = {
+    name: "manthan",
+  };
+  const name = user.name;
+  return username + name;
 }
 
 login("manthan");
