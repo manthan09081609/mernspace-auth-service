246f83e918c98e360190c220fef1a1f25d5f325e
Manthan Sharma <mayank9870866130@gmail.com>
2024-04-03 00:09:59 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
   const PORT = Config.PORT;
 
   try {
     app.listen(PORT, () =>
-      // eslint-disable-next-line no-console
-      console.log(`Server Listening on http://localhost:${PORT}`),
+      logger.info(`Server Listening on http://localhost:${PORT}`),
     );
   } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
-    process.exit(1);
+    if (err instanceof Error) {
+      logger.error(err);
+      setTimeout(() => {
+        process.exit(1);
+      }, 1000);
+    }
   }
 };
 
