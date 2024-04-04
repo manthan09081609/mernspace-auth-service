cc0f9880b1df12ec1077b367c9bb718c19fb7e96
Manthan Sharma <mayank9870866130@gmail.com>
2024-04-04 15:33:59 +0530

    add typeorm & database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,20 @@
+import path from "path";
+
 import { config } from "dotenv";
 
-config();
+config({
+  path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
+});
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_NAME, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD } =
+  process.env;
 
 export const Config = {
   PORT,
   NODE_ENV,
+  DB_NAME,
+  DB_HOST,
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
 };
