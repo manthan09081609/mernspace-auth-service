64ce04d11ecfc26fe126caba7f3f4d899dace69a
Manthan Sharma <mayank9870866130@gmail.com>
2024-04-06 02:34:03 +0530

    logger fix

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -6,13 +6,13 @@ const logger = winston.createLogger({
   defaultMeta: {
     serviceName: "auth-service",
   },
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
   transports: [
     new winston.transports.Console({
       level: "info",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
       silent: Config.NODE_ENV === "test",
     }),
     new winston.transports.File({
@@ -20,20 +20,13 @@ const logger = winston.createLogger({
       filename: "error.log",
       level: "error",
 
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
       silent: Config.NODE_ENV === "test",
     }),
     new winston.transports.File({
       dirname: "logs",
       filename: "combined.log",
       level: "info",
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json(),
-      ),
+
       silent: Config.NODE_ENV === "test",
     }),
   ],
