c85ec49d6db4ec84c2751d97a9ce30e5014bc708
Manthan Sharma <mayank9870866130@gmail.com>
2024-04-07 01:23:45 +0530

    add tenant entity & migration

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -2,9 +2,7 @@ import "reflect-metadata";
 
 import { DataSource } from "typeorm";
 
-import { User } from "../entity/User";
 import { Config } from ".";
-import { RefreshToken } from "../entity/RefreshToken";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -16,7 +14,7 @@ export const AppDataSource = new DataSource({
   // Don't use this in Production,Always keep false
   synchronize: false,
   logging: false,
-  entities: [User, RefreshToken],
+  entities: ["src/entity/*.ts"],
   migrations: ["src/migration/*.ts"],
   subscribers: [],
 });
